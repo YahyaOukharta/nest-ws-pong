6bdd332205c87f14477d2d0c6b44ef7916785e97
Yahya Oukharta <youkhart@e1r6p1.1337.ma>
2022-05-23 10:07:35 +0100

    first

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+import { AppGateway } from './app.gateway';
+
+@Module({
+  imports: [],
+  controllers: [AppController],
+  providers: [AppService, AppGateway],
+})
+export class AppModule {}
---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import { AppModule } from './app.module';
+import { join } from 'path';
+import * as fst from "@fastify/static"
+async function bootstrap() {
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter()
+    );
+    app.useStaticAssets({
+      root :join(__dirname, '..', 'static')
+    });
+
+  await app.listen(process.env.PORT || 3001);
+}
+bootstrap();
