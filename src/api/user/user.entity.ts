37832886c77f541346590563a183ca9dab76e6aa
Yahya Oukharta <youkhart@e1r6p3.1337.ma>
2022-05-29 15:55:04 +0100

    use sqlite instead of postgres server..

--- src/api/user/user.entity.ts
+++ src/api/user/user.entity.ts
@@ -1,6 +1,11 @@
 import { Exclude } from 'class-transformer';
 import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
+import { TypeOrmConfigService } from '@/shared/typeorm/typeorm.service';
+
+const db = new TypeOrmConfigService().createTypeOrmOptions().type
+
+
 @Entity()
 export class User extends BaseEntity {
   @PrimaryGeneratedColumn()
@@ -16,6 +21,6 @@ export class User extends BaseEntity {
   @Column({ type: 'varchar', nullable: true })
   public name: string | null;
 
-  @Column({ type: 'timestamp', nullable: true, default: null })
+  @Column({ type: (db==="sqlite" ? 'datetime' : 'timestamp'), nullable: true, default: null })
   public lastLoginAt: Date | null;
 }
--- src/shared/typeorm/typeorm.service.ts
+++ src/shared/typeorm/typeorm.service.ts
@@ -8,18 +8,23 @@ export class TypeOrmConfigService implements TypeOrmConfigService {
     private readonly config: ConfigService;
 
     public createTypeOrmOptions(): TypeOrmModuleOptions {
+        // return {
+        //     type: 'postgres',
+        //     host: this.config.get<string>('DATABASE_HOST'),
+        //     port: this.config.get<number>('DATABASE_PORT'),
+        //     database: this.config.get<string>('DATABASE_NAME'),
+        //     username: this.config.get<string>('DATABASE_USER'),
+        //     password: this.config.get<string>('DATABASE_PASSWORD'),
+
+        // };
         return {
-            type: 'postgres',
-            host: this.config.get<string>('DATABASE_HOST'),
-            port: this.config.get<number>('DATABASE_PORT'),
-            database: this.config.get<string>('DATABASE_NAME'),
-            username: this.config.get<string>('DATABASE_USER'),
-            password: this.config.get<string>('DATABASE_PASSWORD'),
+            type :"sqlite",
+            database: "./dbfile",
             entities: ['dist/**/*.entity.{ts,js}'],
             migrations: ['dist/migrations/*.{ts,js}'],
             migrationsTableName: 'typeorm_migrations',
             logger: 'file',
             synchronize: true, // never use TRUE in production!
-        };
+        }
     }
 }
