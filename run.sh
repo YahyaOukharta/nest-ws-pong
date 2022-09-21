57e61fc1bcd52dba8f04de48c7da698253116447
keddib@student.1337.ma <keddib@student.1337.ma>
2022-09-21 15:26:15 +0100

    update docker file

--- run.sh
+++ run.sh
@@ -1,1 +1,14 @@
-npm run start:dev
+if [ -d "node_modules" ]; then
+  rm -rf node_modules dist
+fi
+
+npm install
+
+if [ "$ENVIRONMENT" == "production" ]; then
+  echo 'Run Production';
+  npm run build
+  npm run start:prod
+else
+  echo 'Run Development';
+  npm run start:dev
+fi
